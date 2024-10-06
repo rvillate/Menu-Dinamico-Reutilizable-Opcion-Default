198f59b314e4275265d34e65e949ed108331dc79
DESKTOP-Q86BB0O\DevraYa <raulvillate15@gmail.com>
2024-10-06 10:52:51 -0500

    Agregar archivos de proyecto.

---
+++ Menu interactivo/MenuDinamico.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menu_interactivo
+{
+    internal class MenuDinamico
+    {
+        public int CrearMenu(string titulo, String[] opciones, int seleccionDefault = -1)
+        {
+            Console.CursorVisible = false;
+
+            if (Console.CursorTop != 0)
+            {
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(titulo + (seleccionDefault != -1 ? $" (Por defecto {seleccionDefault})" : "") + ":");
+            seleccionDefault = seleccionDefault == -1 ? 0 : seleccionDefault-1;
+            int y = Console.CursorTop;
+            int cont = 0;
+
+            int cursorInicio = Console.CursorTop;
+
+            ConsoleKeyInfo accionKey = new ConsoleKeyInfo();
+            do
+            {
+                Console.CursorTop = cursorInicio;
+                cont = 0;
+
+                if (accionKey.Key == ConsoleKey.UpArrow)
+                {
+                    seleccionDefault--;
+                }
+                else if (accionKey.Key == ConsoleKey.DownArrow)
+                {
+                    seleccionDefault++;
+                }
+
+                if (seleccionDefault > opciones.Length - 1)
+                {
+                    seleccionDefault = 0;
+
+                }
+                else if (seleccionDefault < 0)
+                {
+                    seleccionDefault = opciones.Length - 1;
+                }
+                Array.ForEach(opciones, opcion =>
+                {
+                    if (cont == seleccionDefault)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.WriteLine($"-> {opcion}");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"   {opcion}");
+                    }
+                    cont++;
+                });
+            } while ((accionKey = Console.ReadKey(true)).Key != ConsoleKey.Enter);
+
+
+            seleccionDefault++; // index + 1 para correccion
+            Console.WriteLine($"\nOpcion seleccionada: {opciones[seleccionDefault - 1]}\n");
+
+            return seleccionDefault;
+        }
+    }
+}
---
+++ Menu interactivo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Menu_interactivo;
+
+namespace MenuInteractivo
+{
+    class Program
+    {
+        
+        static void Main()
+        {
+            MenuDinamico menu = new MenuDinamico();
+            int opcion = menu.CrearMenu("Seleccione una", new string[] {"1 - Subir", "2 - Bajar", "3 - Eliminar", "4 - Nada" });
+
+            switch (opcion)
+            {
+                case 1:
+                    Console.WriteLine("Logica para una accion de Subir");
+                    break;
+                case 2:
+                    Console.WriteLine("Logica para una accion de Bajar");
+                    break;
+                case 3:
+                    Console.WriteLine("Logica para una accion de Eliminar");
+                    break;
+                case 4:
+                    Console.WriteLine("Logica para una accion de no hacer Nada");
+                    break;
+                default:
+                    break;
+            }
+            opcion = menu.CrearMenu("Seleccione otra", new string[] { "1 - Subir", "2 - Bajar", "3 - Eliminar", "4 - Nada" }, 3);
+        }
+    }
+
+}
